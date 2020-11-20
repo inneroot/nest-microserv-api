30375c2de7c473c702c165234d3dbec18cad569d
inneroot <hakooplay@gmail.com>
2020-11-20 13:38:22 +0300

    tcp working

--- src/app.service.ts
+++ src/app.service.ts
@@ -1,8 +1,26 @@
 import { Injectable } from '@nestjs/common';
+import { Logger } from '@nestjs/common';
+import { ClientOptions, ClientProxy, ClientProxyFactory, Transport } from '@nestjs/microservices';
+
+const microserviceOptions: ClientOptions = {
+  transport: Transport.TCP,
+  options: {
+    host: '127.0.0.1',
+    port: 8877
+  },
+}
 
 @Injectable()
 export class AppService {
-  getHello(): string {
-    return 'Hello World!';
+  private client: ClientProxy;
+  private logger = new Logger('API service')
+
+  constructor(){
+    this.client = ClientProxyFactory.create(microserviceOptions)
+  }
+
+  async getResponse(data: string) {
+    this.logger.log(`getResponse: ${data}`)
+    return await this.client.send<string, string>('message', 'hello from API')
   }
 }
